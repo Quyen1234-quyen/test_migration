cd0d3c33389ae866360517e8544b038f7151d159
Quyen Le Van <quyenlevan97@gamil.com>
2021-06-29 14:14:58 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
+
+createConnection({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "admin",
+    database: "test",
+    entities: [
+        __dirname + "/entity/*.js"
+    ],
+    synchronize: true,
+}).then(connection => {
+    // here you can start to work with your entities
+}).catch(error => console.log(error));
+
+
